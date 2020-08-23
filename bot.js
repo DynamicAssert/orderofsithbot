c81909ee2d4ce698c5169c565da81cc53ebf4549
DynamicAssert <62639750+DynamicAssert@users.noreply.github.com>
2020-08-23 15:36:32 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const port = 3000;
 const noblox = require("noblox.js");
 const app = express();
 const groupID = 2931379;
-const key = "DO_NOT_SHARE_THIS_KEY_IT_ALLOWS_PEOPLE_TO_TAKE_ADVANTAGE_OF_BOT_MODERATION_FEATURES_35623162625409672457854029728547829457249754272495878457984randomletterspeopleprobablywonteverguess464363456n5426i2456un2453g6i4256jng456j453gn62435j6gnjg62643hngjhgn62hjngjh6njh";
+const key = "";
 
 async function run() {
 	await noblox.setCookie("_|:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|");
